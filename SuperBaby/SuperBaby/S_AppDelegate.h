836c0ebb4a98735767f2cf893262a22fe70a5b5a
Chintan Ramani <MAC107@Chintan.local>
2015-03-25 11:54:38 +0530

    - Added SuperBaby SuperPack to purchase all videos.

--- SuperBaby/SuperBaby/S_AppDelegate.h
+++ SuperBaby/SuperBaby/S_AppDelegate.h
@@ -9,12 +9,16 @@
 #import <UIKit/UIKit.h>
 #import "Mobiquire.h"
 typedef void (^UNAuthorizedBlock)(BOOL success);
+
+typedef void (^SuperpackBlock)(BOOL isSuperPack);
+
 #define NSLog(...)
 
 @class S_RegisterVC;
 @interface S_AppDelegate : UIResponder <UIApplicationDelegate, MobiquireDelegate>
 {
     UNAuthorizedBlock unauthorizeCallback;
+    SuperpackBlock superPackCallBack;
 }
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) UINavigationController *navC;
@@ -25,6 +29,7 @@ typedef void (^UNAuthorizedBlock)(BOOL success);
 -(void)addMilestoneToTimeline_WatchVideo:(NSDictionary *)dictTemp withVideoID:(NSString *)strVideoID;
 -(void)sendFacebook:(UIViewController *)vc with_Text:(NSString *)strText withLink:(NSString *)strLink;
 
+- (void)display_SuperPack_withPrice:(NSString *)strPrice withViewController:(UIViewController*)viewCtr withSuperpackHandler:(SuperpackBlock)superPackBlock;
 - (void)display_UNAuthorized_AlertwithTitle:(NSString*)title withViewController:(UIViewController*)viewCtr withHandler:(UNAuthorizedBlock)complition;
 
 @end
